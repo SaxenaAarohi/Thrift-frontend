e648044910d1331196cc8b7ad7861116b13b05c6
SaxenaAarohi <aarohisaxena1014@gmail.com>
2026-01-06 13:22:01 +0530

    Remove path prop from SignUp component

--- src/components/Sign_up.tsx
+++ src/components/Sign_up.tsx
@@ -5,7 +5,6 @@ export default function Sign_up() {
     <div className="flex my-4 justify-center items-center">
       <div>
         <SignUp
-          path="/signup"
           forceRedirectUrl="/complete_profile" />
       </div>
 
